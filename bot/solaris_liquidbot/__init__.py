ab2c8dae56c9513e66107a696ec9f035db452543
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-08 02:25:13 +0300

    solaris_liquidbot: add get_unhealthy_obligations()

--- bot/solaris_liquidbot/__init__.py
+++ bot/solaris_liquidbot/__init__.py
@@ -1,25 +1,63 @@
 from construct import Int8ub, Int64ub, Struct, Array, Byte, Bytes, Container
+import requests
+import json
+import base64
 
 PUBLIC_KEY_LAYOUT = Bytes(32)
-OBLIGATION_LAYOUT_LEN = 265
+DECIMAL = Array(3, Int64ub)
+
+OBLIGATION_LAYOUT_LEN = 916
+TOKEN_LENDING_PROGRAM_ID = '6h5geweHee42FbxZrYAcYJ8SGVAjG6sGow5dtzcKtrJw'
 
 class LiquidBot:
-    __format = Struct(
-        "version" / Int8ub,
-        "deposited_collateral_tokens" / Int64ub,
-        "collateral_reserve" / PUBLIC_KEY_LAYOUT,
-        "cumulative_borrow_rate_wads" / Array(3, Int64ub),
-        "borrowed_liquidity_wads" / Array(3, Int64ub),
-        "borrow_reserve" / PUBLIC_KEY_LAYOUT,
-        "token_mint" / PUBLIC_KEY_LAYOUT
-    )
+    __url = 'https://api.devnet.solana.com'
 
     """ This is LiquidBot Class """
     def __init__(self, threaded=True):
         self.threaded = threaded
 
-    def deserialize_obligation(self, data: bytes) -> Container:
-        if len(data) != OBLIGATION_LAYOUT_LEN:
-            raise ValueError("Invalid data len")
+    def get_obligaions(self) -> dict:
+        headers = {'Content-Type': 'application/json'}
+        body = json.dumps({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "getProgramAccounts",
+            "params": [
+                TOKEN_LENDING_PROGRAM_ID,
+                {
+                    "encoding": "jsonParsed",
+                    "filters": [
+                        {
+                            "dataSize": OBLIGATION_LAYOUT_LEN
+                        }
+                    ]
+                }
+            ]
+        })
 
-        return self.__format.parse(data)
+        responce = requests.post(self.__url, headers=headers, data=body)
+        if responce.status_code != 200:
+            raise ValueError("Invalid request")
+
+        return json.loads(responce.text).get('result')
+
+    def get_unhealthy_obligations(self):
+        unhealthy_obligations = []
+        
+        for obligation in self.get_obligaions():
+            data = obligation.get('account').get('data')
+            if type(data) == list and len(data) > 0:
+                data = data[0]
+
+            data = base64.b64decode(data)
+            
+            borrowed_data = data[1 + 8 + 1 + 32 + 32 + 16:][:16]
+            borrowed_value = int.from_bytes(borrowed_data, "little")
+
+            unhealthy_borrow_data = data[1 + 8 + 1 + 32 + 32 + 16 + 16 + 16:][:16]
+            unhealthy_borrow_value = int.from_bytes(unhealthy_borrow_data, "little")
+
+            if borrowed_value < unhealthy_borrow_value:
+                print('Obligation is healthy and cannot be liquidated')
+
+        return unhealthy_obligations
