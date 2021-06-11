2b6a7aad2b68f8619429f0c092d0c5a6c33b0da7
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-11 15:20:05 +0300

    test.py: updated

--- bot/test.py
+++ bot/test.py
@@ -1,5 +1,13 @@
-import base64
+import os
+import json
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 from solaris_liquidbot import LiquidBot
 
-bot = LiquidBot()
-unhealthy_obligations = bot.get_unhealthy_obligations()
+url = 'https://api.devnet.solana.com'
+payer_keypair = json.loads(os.getenv('PAYER_KEYPAIR'))
+
+bot = LiquidBot(url, payer_keypair)
+bot.check_and_liquidate_unhealthy_obligations()
