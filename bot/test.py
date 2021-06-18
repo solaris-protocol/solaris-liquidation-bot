e9b766afc6330da7797a467e4b2d5882a1129e65
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-18 18:16:25 +0300

    test updated

--- bot/test.py
+++ bot/test.py
@@ -6,8 +6,19 @@ load_dotenv(find_dotenv())
 
 from solaris_liquidbot import LiquidBot
 
-url = 'https://api.devnet.solana.com'
-payer_keypair = json.loads(os.getenv('PAYER_KEYPAIR'))
+def test_check_and_liquidate_unhealthy_obligations():
+    url = 'https://api.devnet.solana.com'
+    payer_keypair = json.loads(os.getenv('PAYER_KEYPAIR'))
 
-bot = LiquidBot(url, payer_keypair)
-bot.check_and_liquidate_unhealthy_obligations()
+    token_lending_program_address = os.getenv('TOKEN_LENDING_PROGRAM_ADDRESS')
+    bot = LiquidBot(url, payer_keypair, token_lending_program_address)
+    bot.check_and_liquidate_unhealthy_obligations()
+
+def test_get_obligaions():
+    url = 'https://api.devnet.solana.com'
+    payer_keypair = json.loads(os.getenv('PAYER_KEYPAIR'))
+
+    token_lending_program_address = os.getenv('TOKEN_LENDING_PROGRAM_ADDRESS')
+    bot = LiquidBot(url, payer_keypair, token_lending_program_address)
+    obligaions = bot.get_obligaions()
+    print(obligaions)
